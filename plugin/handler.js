0b6bbf22ff90a3b99f5e3517ec766d556f5b1d48
tsunima <f0230074@gmail.com>
2015-10-18 04:07:24 +0900

    tmp commit

--- plugin/handler.js
+++ plugin/handler.js
@@ -4,7 +4,7 @@
 const ts = Npm.require('typescript');
 const fse = Npm.require('fs-extra');
 const _ = Npm.require('lodash');
-var debug = Npm.require('debug')('ts:debug:');
+// const debug = Npm.require('debug')('ts:debug:');
 Plugin.registerCompiler({
     extensions: ['d.ts', 'ts', 'tsx'],
     filenames: []
@@ -14,19 +14,45 @@ Plugin.registerCompiler({
 
 class Compiler {
     constructor() {
-        this.cache = new Map;
-        this.complieFiles = [];
-        this.emitSkipped = false;
+        /**
+         * cache for each host
+         */
+        this.cache = {};
+
+        /**
+         * language services
+         */
+        this.services = {};
+
+        /**
+         * language services host
+         */
+        this.hostFiles = {};
+
+        /**
+         * shared registry document registry
+         */
+        this.documentRegistry = ts.createDocumentRegistry();
+
+        /**
+         * compiler options
+         */
         this.options = this.options || {
             noEmitOnError: false,
             sourceMap: true,
             emitDecoratorMetadata: true,
             experimentalDecorators: true,
+            isolatedModules: false,
+            allowNonTsExtensions: true,
+            declaration: false,
             jsx: ts.JsxEmit.React,
             target: ts.ScriptTarget.ES5,
             module: ts.ModuleKind.None
         };
 
+        /**
+         * init compiler options
+         */
         let options = fse.readJsonSync('tsconfig.json', {
             throws: false
         });
@@ -36,71 +62,107 @@ class Compiler {
             this.parserOptions(options.compilerOptions);
         }
 
-        // init language services
-        this.services = this.createServices();
+        /**
+         * init language services and cache
+         */
+        this.services['server'] = this.createServices('server');
+        this.cache['server'] = new Map;
+        this.services['web.browser'] = this.createServices('web.browser');
+        this.cache['web.browser'] = new Map;
+        let platforms = fse.readFileSync('.meteor/platforms', 'utf8');
+        if (platforms.search('ios') !== -1 || platforms.search('android') !== -1) {
+            this.services['web.cordova'] = this.createServices('web.cordova');
+            this.cache['web.cordova'] = new Map;
+        }
 
         // starting message
         msg[2](' Using Typescript Compiler......         ');
     }
 
-    createServices() {
+    createServices(arch) {
         // Create the language service host to allow the LS to communicate with the host
         const servicesHost = {
-            getScriptFileNames: () => this.complieFiles,
-            getScriptVersion: (fileName) => this.cache.get(fileName) !== undefined ? this.cache.get(fileName).version : 'const',
-            getScriptSnapshot: (fileName) => ts.ScriptSnapshot.fromString(ts.sys.readFile(fileName)),
-            getCurrentDirectory: () => ts.sys.getCurrentDirectory(),
+            getScriptFileNames: () => this.hostFiles[arch],
+            getScriptVersion: fileName => this.cache[arch].has(fileName) ? this.cache[arch].get(fileName).version : 'immutable',
+            getScriptSnapshot: fileName => ts.ScriptSnapshot.fromString(ts.sys.readFile(fileName)),
+            getCurrentDirectory: () => '/',
             getCompilationSettings: () => this.options,
-            getDefaultLibFileName: (options) => ts.getDefaultLibFilePath(options),
+            getDefaultLibFileName: options => ts.getDefaultLibFilePath(options)
         };
 
-        return ts.createLanguageService(servicesHost, ts.createDocumentRegistry());
+        return ts.createLanguageService(servicesHost, this.documentRegistry);
     }
 
     // override
     processFilesForTarget(files) {
-        this.complieFiles = [];
-        // push them into `complieFiles`
-        files.forEach((file, index) => {
-            this.cache.set(file.getPathInPackage(), {
-                version: file.getSourceHash(),
-                error: e => file.error(e),
-                addJavaScript: f => file.addJavaScript(f),
-                extension: file.getExtension()
-            });
-            this.complieFiles.push(file.getPathInPackage());
+        let arch = files[0].getArch();
+        if (arch.search('os.') !== -1) {
+            arch = 'server';
+        }
+        this.hostFiles[arch] = [];
+        let compileFiles = []; // need to be comipled
+
+        files.forEach(file => {
+            let fileName = file.getPathInPackage();
+            let hash = file.getSourceHash();
+
+            // push new files into `hostFiles`
+            this.hostFiles[arch].push(fileName);
+
+            if (!this.cache[arch].has(fileName)) {
+
+                // mark file as need compile
+                compileFiles.push(fileName);
+                this.cache[arch].set(fileName, {
+                    version: hash,
+                    // extension: file.getExtension(),
+                    error: e => file.error(e),
+                    addJavaScript: f => file.addJavaScript(f)
+                });
+            } else {
+                if (this.cache[arch].get(fileName).version !== file.getSourceHash()) {
+                    compileFiles.push(fileName);
+                    this.cache[arch].set(fileName, {
+                        version: hash,
+                        // extension: file.getExtension(),
+                        error: e => file.error(e),
+                        addJavaScript: f => file.addJavaScript(f)
+                    });
+                }
+            }
         });
 
         // exec
-        this.complieFiles.forEach(file => {
-            this.emitFile(file);
-        })
+        compileFiles.forEach(file => {
+            this.emitFile(file, arch);
+        });
 
         // logErrors
-        this.logErrors();
+        this.logErrors(arch);
     }
 
-    emitFile(file) {
-        debug('Emit Files: %j', file);
-        let output = this.services.getEmitOutput(file);
-
+    emitFile(file, arch) {
+        // debug('Emit Files: %j', file);
+        let output = this.services[arch].getEmitOutput(file);
+        let moduleName = file.replace(/\.tsx?$/, '').replace(/\\/g, '/');
         if (!output.emitSkipped) {
-            if (output.outputFiles[0] !== undefined) {
-                this.cache.get(file).addJavaScript({
-                    data: output.outputFiles[1].text,
+            if (output.outputFiles.length > 0) {
+                this.cache[arch].get(file).addJavaScript({
+                    data: output.outputFiles[1].text
+                        .replace("System.register([",'System.register("'+moduleName+'",[')
+                        .replace("define([",'define("'+moduleName+'",['),
                     path: output.outputFiles[1].name,
+                    sourcePath: file,
                     sourceMap: output.outputFiles[0].text,
-                    bare: true
-                })
+                    bare: this.options.module ? true : false
+                });
             }
-            return;
         }
-        this.emitSkipped = true;
     }
 
-    logErrors() {
-        let program = this.services.getProgram();
-        let allDiagnostics = this.services.getCompilerOptionsDiagnostics()
+    logErrors(arch) {
+        let program = this.services[arch].getProgram();
+        let allDiagnostics = this.services[arch].getCompilerOptionsDiagnostics()
             .concat(program.getSyntacticDiagnostics())
             .concat(program.getSemanticDiagnostics());
 
@@ -111,9 +173,9 @@ class Compiler {
             } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
 
             // emit error messages
-            if (this.emitSkipped) {
+            if (this.options.noEmitOnError) {
                 // stop the meteor app wait for fix
-                this.cache.get(diagnostic.file.fileName).error({
+                this.cache[arch].get(diagnostic.file.fileName).error({
                     message: message,
                     column: character + 1,
                     line: line + 1
@@ -130,11 +192,11 @@ class Compiler {
         if (_.has(inputOptins, 'module')) {
             let module = inputOptins.module.toLowerCase();
             if (module === 'none') {
-                this.options.module = 0;
+                this.options.module = ts.ModuleKind.None;
             } else if (module === 'system') {
-                this.options.module = 4;
+                this.options.module = ts.ModuleKind.System;
             } else if (module === 'amd') {
-                this.options.module = 2;
+                this.options.module = ts.ModuleKind.AMD;
             } else {
                 msg[0](' Cannot use \"module\": \"' + inputOptins.module + '\" option, \"module\" will be set to \"None\"');
             }
@@ -153,6 +215,11 @@ class Compiler {
         // noLib
         if (_.has(inputOptins, 'noLib')) {
             delete inputOptins.noLib;
+        }
+
+        // declaration
+        if (_.has(inputOptins, 'declaration')) {
+            delete inputOptins.declaration;
         }
 
         // watch
